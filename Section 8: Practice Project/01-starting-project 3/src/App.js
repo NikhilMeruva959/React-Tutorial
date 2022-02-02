7f01905f3ae1aca411532cb4849c2097988f6e0d
Nikhil Meruva <nikmeruva1@gmail.com>
2022-02-02 00:16:26 -0500

    Added Error Modal, but have to fix errors

--- Section 8: Practice Project/01-starting-project 3/src/App.js
+++ Section 8: Practice Project/01-starting-project 3/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UserList';
 
 function App() {
+  const[userList, setUserList] = useState([]);
+
+  const addUserHandler = (userName, userAge) =>{
+    setUserList((prevUsersList) => {
+      return [...prevUsersList, {name: userName, age: userAge, id:Math.random().toString()}];
+    });
+  };
+
+
   return (
     <div>
-      <AddUser></AddUser>
+      <AddUser onAddUser = {addUserHandler}></AddUser>
+      <UsersList users={userList}></UsersList>
     </div>
   );
 }
--- Section 8: Practice Project/01-starting-project 3/src/components/Users/AddUser.js
+++ Section 8: Practice Project/01-starting-project 3/src/components/Users/AddUser.js
@@ -2,34 +2,59 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
 
-  const userNameChangeHandler = (event) =>{
-    setEnteredUserName = event.target.value;
-  }
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
 
-  const ageChangeHandler = (event) =>{
-    setEnteredAge = event.target.value;
-  } 
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredUserName, enteredAge);
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    //+ does string -> int conversion
+    if (+enteredAge < 1) {
+      return;
+    }
+    // console.log(enteredUserName, enteredAge);
+    props.onAddUser(enteredUserName, enteredAge);
+    setEnteredAge("");
+    setEnteredUserName("");
   };
 
   return (
-    <Card cssClass={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={userNameChangeHandler}/>
-        <label htmlFor="age">Age</label>
-        <input id="age" type="number" onChange={ageChangeHandler}/>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      <Card cssClass={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          {/* //value={enteredUserName} resets the form because of setEnteredAge(''); and setEnteredUserName(''); */}
+          <input
+            id="username"
+            type="text"
+            value={enteredUserName}
+            onChange={userNameChangeHandler}
+          />
+          <label htmlFor="age">Age</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 export default AddUser;
