ad92a6082165ef4c60e69ffef4061dd0d7326a14
Nikhil Meruva <nikmeruva1@gmail.com>
2022-02-04 11:54:02 -0500

    useEffect setFormValid()

--- Section 10 - Side Effects, Reducers, & Context API/01-starting-project 4/src/App.js
+++ Section 10 - Side Effects, Reducers, & Context API/01-starting-project 4/src/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+// Executed AFTER every component reevaluation, but only if dependecies change
+  useEffect(() => {
+    const storedUderLoggedInInformation = localStorage.getItem('isLoggedIn');
+    
+    if(storedUderLoggedInInformation === '1'){
+      setIsLoggedIn(true);
+    } 
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
