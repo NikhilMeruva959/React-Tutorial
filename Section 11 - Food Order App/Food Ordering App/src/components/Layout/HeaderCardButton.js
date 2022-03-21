c722dc6be665e74c3763f55b808f29a2270fb00b
Nikhil Meruva <nikmeruva1@gmail.com>
2022-03-21 11:23:57 -0400

    Added CartCtx

--- Section 11 - Food Order App/Food Ordering App/src/components/Layout/HeaderCardButton.js
+++ Section 11 - Food Order App/Food Ordering App/src/components/Layout/HeaderCardButton.js
@@ -1,15 +1,23 @@
 import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCardButton.module.css';
-
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCardButton = (props) => {
+    const cartctx = useContext(CartContext);
+
+    //Reduce alows you to transform array into single value
+    const numCartItems = cartctx.items.reduce((currNumber, item)=>{
+        return currNumber+item.amount;
+    }, 0);
+
     return(
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}><CartIcon/></span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numCartItems}
             </span>
         </button>
 
