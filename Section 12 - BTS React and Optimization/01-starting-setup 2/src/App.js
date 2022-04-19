1d19f014f3a28e564a2e2084f8fcbe0568d86f5b
Nikhil Meruva <nikmeruva1@gmail.com>
2022-04-19 12:20:43 -0400

    Finished Section 12

--- Section 12 - BTS React and Optimization/01-starting-setup 2/src/App.js
+++ Section 12 - BTS React and Optimization/01-starting-setup 2/src/App.js
@@ -1,22 +1,63 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import Button from './components/UI/Button/Button';
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showPara, setShowPara] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
 
   console.log("APP RUNNING");
 
-  const toggleParaHandler = () => {
-    setShowPara(prevShowPara => !prevShowPara);
-  }
+  /*
+  useCallBack: 
+
+  let obj1 = {}
+  let obj2 = {}
+  obj1 != obj2
+  obj1 = obj2  // refrence
+  obj1 === obj2
+
+  - useCallBack saves a function of our choice in React internal storage and 
+  always reuse the same function object again when this component function executes
+
+  */
+  /*
+  - useCallback() will reuse the same function callback
+  - needs an array of dependencies as second arg
+
+  useCallback() Dependencies:
+    - in javscript function are closures (close over values that are availiable in the enviornment)
+    - 
+
+  */
+ /* 
+ // React will not recreate this function because of useCallBack()
+  const toggleParaHandler = useCallback(() => {
+    if(allowToggle){
+      setShowPara(prevShowPara => !prevShowPara);
+    }
+  }, []);
+  */
+
+  //now ill recreate function when allowToggle updates
+  const toggleParaHandler = useCallback(() => {
+    if(allowToggle){
+      setShowPara(prevShowPara => !prevShowPara);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
       {/* Component functions will get reexectued, look at console */}
 
-      <DemoOutput show={false}/>
+      <DemoOutput show={showPara}/>
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParaHandler}>Show Para</Button>
     </div>
   );
--- Section 12 - BTS React and Optimization/01-starting-setup 2/src/components/UI/Button/Button.js
+++ Section 12 - BTS React and Optimization/01-starting-setup 2/src/components/UI/Button/Button.js
@@ -14,5 +14,14 @@ const Button = (props) => {
     </button>
   );
 };
+/*
+-functions are just objects in javascript
+  - new function object is created when the App.js runs
+  - toggleParaHandler is passed into function object as a prop
+  - therefore, button compares props.onClick === props.previous.onClick
+      - REMEBER, in javscript [1,2,3] != [1,2,3]
+      - even if they have the same content, NEVER the same
+      - that is how React.memo knows the values change
+*/
 
 export default React.memo(Button);
